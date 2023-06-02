be3ab90e20aefe7aaeb1cc972cb45df1ce37aea1
mellamanmar <marimelissamc@gmail.com>
2023-06-02 12:15:24 -0500

    Update folders main and config, add new bookstores

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 
-app= FastAPI()
+inventory= FastAPI()
+inventory.title = "Mi app con FastAPI"
+inventory.version = "0.0.1"
 
 
-@app.get ('/')
+app.add_middleware(Errorhandler)
+
+
+
+Base.metadata.create_all(bind=engine)
+
+
+@inventory.get ('/')
 def root():
     return HTMLResponse ('<h1>Esta es la API de Marianny</h1>')
 
-@app.get('/home')
+@inventory.get('/home')
 def home():
     return HTMLResponse('<h1>Hello World</h1>')
+
